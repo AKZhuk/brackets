17686464ea533060fc1afc4caae7ae0d2b4da023
github-AKZhuk <lesha.zhuk@gmail.com>
2021-02-24 15:26:23 +0300

    feat: implement basic case of  brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    bracketsConfig = bracketsConfig.flat();
+    str = str.split("");
+    let stack = [];
+    let result = true;
+    let index;
+
+    for (let n = 0; n < str.length; n++) {
+        index = bracketsConfig.indexOf(str[n]);
+
+        if (index % 2 === 0) {
+            bracketsConfig[index] != bracketsConfig[index + 1]
+                ? stack.push(index + 1)
+                : index;
+        } else {
+            if (stack.pop() !== index) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
